55d10317300c3e10153fcbed509f0adf53cf8ad1
yoav58 <73054420+yoav58@users.noreply.github.com>
2024-12-18 01:24:37 +0200

    update CRM UI

--- crm/flutter_bootstrap.js
+++ crm/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "570398955"
+    serviceWorkerVersion: "2532740972"
   }
 });
